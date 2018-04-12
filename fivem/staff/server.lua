95f4c1a9f5e5d7baa47534e37a4deaa0e33074b2
Avery Johnson <avery627@gmail.com>
2018-04-12 11:28:32 -0400

    Update server.lua

--- fivem/staff/server.lua
+++ fivem/staff/server.lua
@@ -12,9 +12,9 @@ local website = "https://arthurmitchell.xyz/beta"
 ------- DO NOT EDIT BELOW THIS LINE -------
 AddEventHandler( "playerConnecting", function(name, setReason, deferrals)
 	if string.find(GetPlayerIdentifiers(source)[1], "steam:") then
-		PerformHttpRequest(website .. '/api/adduser?name=' .. GetPlayerName(source) .. '&license=' .. GetPlayerIdentifiers(source)[2], function(statusCode, response, headers) end)
 		deferrals.defer()
 		deferrals.update("Checking Player Information. Please Wait.")
+		PerformHttpRequest(website .. '/api/adduser?name=' .. GetPlayerName(source) .. '&license=' .. GetPlayerIdentifiers(source)[2], function(statusCode, response, headers) end)
 		PerformHttpRequest(website .. '/api/checkban?license=' .. GetPlayerIdentifiers(source)[2], function(statusCode, response, headers)
 			if response then
 				local userinfo = json.decode(response)
