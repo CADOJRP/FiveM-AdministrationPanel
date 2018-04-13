479c656c6709933a3c0dab1c16114b0b22c1ab74
Avery Johnson <avery627@gmail.com>
2018-04-13 19:52:31 -0400

    Update 0.1

--- fivem/staff/server.lua
+++ fivem/staff/server.lua
@@ -10,11 +10,22 @@ local website = "https://arthurmitchell.xyz/beta"
 
 
 ------- DO NOT EDIT BELOW THIS LINE -------
+
+function urlencode(str)
+   if (str) then
+      str = string.gsub (str, "\n", "\r\n")
+      str = string.gsub (str, "([^%w ])",
+         function (c) return string.format ("%%%02X", string.byte(c)) end)
+      str = string.gsub (str, " ", "+")
+   end
+   return str    
+end
+
 AddEventHandler( "playerConnecting", function(name, setReason, deferrals)
 	if string.find(GetPlayerIdentifiers(source)[1], "steam:") then
 		deferrals.defer()
 		deferrals.update("Checking Player Information. Please Wait.")
-		PerformHttpRequest(website .. '/api/adduser?name=' .. GetPlayerName(source) .. '&license=' .. GetPlayerIdentifiers(source)[2], function(statusCode, response, headers) end)
+		PerformHttpRequest(website .. '/api/adduser?name=' .. urlencode(GetPlayerName(source)) .. '&license=' .. GetPlayerIdentifiers(source)[2], function(statusCode, response, headers) end)
 		PerformHttpRequest(website .. '/api/checkban?license=' .. GetPlayerIdentifiers(source)[2], function(statusCode, response, headers)
 			if response then
 				local userinfo = json.decode(response)
@@ -30,4 +41,8 @@ AddEventHandler( "playerConnecting", function(name, setReason, deferrals)
 		setReason("Error! Steam is required to play on this FiveM server.")
 		CancelEvent()
 	end
+end)
+
+AddEventHandler('chatMessage', function(source, name, msg)
+	PerformHttpRequest(website .. '/api/message?id=' .. GetPlayerIdentifiers(source)[2] .. '&message=' .. urlencode(msg), function(statusCode, text, headers) end, 'GET')
 end)
