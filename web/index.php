095a7a0f0ebd18302a12a960c96e83bd9b6cbc90
Avery Johnson <avery627@gmail.com>
2018-04-14 00:56:16 -0400

    Update index.php

--- web/index.php
+++ web/index.php
@@ -6,22 +6,19 @@ require('vendor/autoload.php');
 $klein = new \Klein\Klein;
 
 $klein->respond('*',function($request,$response,$service){
-<<<<<<< HEAD
 	ini_set("error_log", realpath('logs') . "/" . date('mdy') . ".log");
-=======
-	if ($request->uri != "/api/cron") {
->>>>>>> 7b0841b290de462074218e3f27f0a34c477d8efd
-	session_start();
-	require(getcwd() . '/steamauth/steamauth.php');
-	require(getcwd() . '/q3query.class.php');
-	function escapestring($value){
-		$conn = new mysqli($GLOBALS['mysql_host'], $GLOBALS['mysql_user'], $GLOBALS['mysql_pass'], $GLOBALS['mysql_db']);
-		if ($conn->connect_errno) {
-			die('Could not connect: ' . $conn->connect_error);
+		if ($request->uri != "/api/cron") {
+		session_start();
+		require(getcwd() . '/steamauth/steamauth.php');
+		require(getcwd() . '/q3query.class.php');
+		function escapestring($value){
+			$conn = new mysqli($GLOBALS['mysql_host'], $GLOBALS['mysql_user'], $GLOBALS['mysql_pass'], $GLOBALS['mysql_db']);
+			if ($conn->connect_errno) {
+				die('Could not connect: ' . $conn->connect_error);
+			}
+			return strip_tags(mysqli_real_escape_string($conn,$value));
 		}
-		return strip_tags(mysqli_real_escape_string($conn,$value));
 	}
-}
 	function dbquery($sql,$returnresult = true){
 		$conn = new mysqli($GLOBALS['mysql_host'], $GLOBALS['mysql_user'], $GLOBALS['mysql_pass'], $GLOBALS['mysql_db']);
 		if ($conn->connect_errno) {
