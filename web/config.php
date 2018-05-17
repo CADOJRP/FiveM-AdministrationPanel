cec129a9cf43c9d432786282efc67167bbe459db
Avery Johnson <avery627@gmail.com>
2018-05-17 18:00:28 -0400

    Updated Wrong Permission Value

--- web/config.php
+++ web/config.php
@@ -34,7 +34,7 @@
 			"warn",
 			"kick",
 			"ban",
-			"unban",
+			"delrecord",
 			"editstaff",
 			"editservers"
 		],
@@ -42,7 +42,7 @@
 			"warn",
 			"kick",
 			"ban",
-			"unban"
+			"delrecord"
 		],
 		"admin"=> [
 			"warn",
