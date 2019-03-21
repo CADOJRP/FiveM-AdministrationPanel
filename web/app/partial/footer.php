8d5afd145e25c66be24db1da65e03dbc2177f437
Avery Johnson <avery627@gmail.com>
2019-03-21 16:48:12 -0400

    Debug Load Time

--- web/app/partial/footer.php
+++ web/app/partial/footer.php
@@ -1,6 +1,9 @@
 </div>
 </body>
     <!--<script src="//code.jquery.com/jquery-1.9.1.js"></script>-->
+    <script type="text/javascript">
+        console.log("Page Load Time: <?php echo round((microtime(true) - $GLOBALS['time_start']), 4); ?>");
+    </script>
     <script src="//code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
     <script src="<?php echo $GLOBALS['domainname']; ?>app/js/bootstrap.min.js" type="text/javascript"></script>
     <script src="<?php echo $GLOBALS['domainname']; ?>app/js/bootstrap-checkbox-radio-switch.js"></script>
