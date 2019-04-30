fbf0a37e69678652dd99f110ab76b42a8210a7fb
DXNCXX <40762618+DXNCXX@users.noreply.github.com>
2019-04-30 22:52:48 +0100

    More Secure Bans. URGENT UPDATE

--- web/fivemad1_panel.sql
+++ web/fivemad1_panel.sql
@@ -1,83 +1,94 @@
--- phpMyAdmin SQL Dump
--- version 4.8.4
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Mar 23, 2019 at 07:25 PM
--- Server version: 10.1.37-MariaDB
--- PHP Version: 7.3.0
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `fivemad1_panel`
---
-
--- --------------------------------------------------------
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
 -- Table structure for table `bans`
 --
 
+DROP TABLE IF EXISTS `bans`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `bans` (
-  `ID` int(255) NOT NULL,
+  `ID` int(255) NOT NULL AUTO_INCREMENT,
   `name` varchar(1024) NOT NULL,
   `identifier` varchar(1024) NOT NULL,
+  `steam` varchar(255) DEFAULT '',
+  `discord` varchar(255) DEFAULT '',
+  `xbl` varchar(255) DEFAULT '',
+  `ip` varchar(255) DEFAULT '',
+  `live` varchar(255) DEFAULT '',
   `reason` varchar(1024) NOT NULL,
   `ban_issued` varchar(1024) NOT NULL,
   `banned_until` varchar(1024) NOT NULL,
   `staff_name` varchar(1024) NOT NULL,
   `staff_steamid` varchar(1024) NOT NULL,
-  `community` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `community` varchar(255) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=MyISAM AUTO_INCREMENT=26266 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+
 
 --
 -- Table structure for table `commend`
 --
 
+DROP TABLE IF EXISTS `commend`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `commend` (
-  `ID` int(255) NOT NULL,
+  `ID` int(255) NOT NULL AUTO_INCREMENT,
   `license` varchar(255) NOT NULL,
   `reason` varchar(1024) NOT NULL,
   `staff_name` varchar(255) NOT NULL,
   `staff_steamid` varchar(255) NOT NULL,
   `time` varchar(255) NOT NULL,
-  `community` varchar(255) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `community` varchar(255) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=MyISAM AUTO_INCREMENT=5232 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+
 
 --
 -- Table structure for table `communities`
 --
 
+DROP TABLE IF EXISTS `communities`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `communities` (
-  `ID` int(255) NOT NULL,
+  `ID` int(255) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   `owner` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL DEFAULT '',
   `time` int(11) NOT NULL,
-  `uniqueid` varchar(255) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `uniqueid` varchar(255) NOT NULL,
+  `active` tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`ID`)
+) ENGINE=MyISAM AUTO_INCREMENT=1799 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+
 
 --
 -- Table structure for table `config`
 --
 
+DROP TABLE IF EXISTS `config`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `config` (
-  `ID` int(255) NOT NULL,
+  `ID` int(255) NOT NULL AUTO_INCREMENT,
   `community_name` varchar(255) NOT NULL DEFAULT 'Community Name',
   `discord_webhook` varchar(1024) DEFAULT NULL,
   `joinmessages` enum('true','false') NOT NULL DEFAULT 'false',
@@ -96,224 +107,191 @@ CREATE TABLE `config` (
   `debug` enum('false','true') NOT NULL DEFAULT 'false',
   `community` varchar(255) NOT NULL,
   `plugin_extendeduserinfo` tinyint(1) NOT NULL DEFAULT '0',
-  `plugin_globaluserinfo` tinyint(1) NOT NULL DEFAULT '0'
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `plugin_globaluserinfo` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`ID`)
+) ENGINE=MyISAM AUTO_INCREMENT=1796 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+
 
 --
 -- Table structure for table `kicks`
 --
 
+DROP TABLE IF EXISTS `kicks`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `kicks` (
-  `ID` int(255) NOT NULL,
+  `ID` int(255) NOT NULL AUTO_INCREMENT,
   `license` varchar(1024) NOT NULL,
   `reason` varchar(1024) NOT NULL,
   `staff_name` varchar(255) NOT NULL,
   `staff_steamid` varchar(255) NOT NULL,
   `time` varchar(255) NOT NULL,
-  `community` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `community` varchar(255) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=MyISAM AUTO_INCREMENT=60358 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+
 
 --
 -- Table structure for table `notes`
 --
 
+DROP TABLE IF EXISTS `notes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `notes` (
-  `ID` int(255) NOT NULL,
+  `ID` int(255) NOT NULL AUTO_INCREMENT,
   `license` varchar(255) NOT NULL,
   `reason` varchar(255) NOT NULL,
   `staff_name` varchar(255) NOT NULL,
   `staff_steamid` varchar(255) NOT NULL,
   `time` varchar(255) NOT NULL,
-  `community` varchar(255) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `community` varchar(255) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=MyISAM AUTO_INCREMENT=1533 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
 
 --
 -- Table structure for table `players`
 --
 
+DROP TABLE IF EXISTS `players`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `players` (
-  `ID` int(255) NOT NULL,
+  `ID` int(255) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   `license` varchar(255) NOT NULL,
   `steam` varchar(255) NOT NULL,
   `discord` varchar(255) DEFAULT NULL,
-  `playtime` int(255) NOT NULL DEFAULT '1',
+  `xbl` varchar(255) DEFAULT NULL,
+  `ip` varchar(255) DEFAULT NULL,
+  `live` varchar(255) DEFAULT NULL,
+  `playtime` int(255) NOT NULL DEFAULT '0',
   `firstjoined` varchar(255) NOT NULL,
   `lastplayed` varchar(255) NOT NULL,
-  `community` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `community` varchar(255) NOT NULL,
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `license` (`license`,`community`)
+) ENGINE=MyISAM AUTO_INCREMENT=42669341 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+
 
 --
 -- Table structure for table `servers`
 --
 
+DROP TABLE IF EXISTS `servers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `servers` (
-  `ID` int(255) NOT NULL,
+  `ID` int(255) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   `connection` varchar(255) NOT NULL,
   `rcon` varchar(255) NOT NULL,
-  `community` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `players` int(255) NOT NULL DEFAULT '0',
+  `community` varchar(255) NOT NULL,
+  `active` tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`ID`)
+) ENGINE=MyISAM AUTO_INCREMENT=1772 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+
 
 --
 -- Table structure for table `support_comments`
 --
 
+DROP TABLE IF EXISTS `support_comments`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `support_comments` (
-  `ID` int(255) NOT NULL,
+  `ID` int(255) NOT NULL AUTO_INCREMENT,
   `message` varchar(2048) NOT NULL,
   `ticketid` varchar(255) NOT NULL,
   `commentid` varchar(255) NOT NULL,
   `steamid` varchar(255) NOT NULL,
-  `time` varchar(255) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `time` varchar(255) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=MyISAM AUTO_INCREMENT=56 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+
 
 --
 -- Table structure for table `support_tickets`
 --
 
+DROP TABLE IF EXISTS `support_tickets`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `support_tickets` (
-  `ID` int(255) NOT NULL,
+  `ID` int(255) NOT NULL AUTO_INCREMENT,
   `title` varchar(1024) NOT NULL,
   `message` varchar(2048) NOT NULL,
   `ticketid` varchar(255) NOT NULL,
   `steamid` varchar(255) NOT NULL,
   `status` enum('open','in-progress','pending','closed') NOT NULL DEFAULT 'open',
-  `time` varchar(255) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `time` varchar(255) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=MyISAM AUTO_INCREMENT=71 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
 
 --
 -- Table structure for table `users`
 --
 
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `users` (
-  `ID` int(255) NOT NULL,
+  `ID` int(255) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   `steamid` varchar(255) NOT NULL,
   `rank` varchar(255) NOT NULL DEFAULT 'user',
   `community` varchar(255) NOT NULL DEFAULT '',
   `staff` tinyint(1) NOT NULL DEFAULT '0',
-  `beta` tinyint(1) NOT NULL DEFAULT '0'
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `beta` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `steamid` (`steamid`),
+  FULLTEXT KEY `rank` (`rank`)
+) ENGINE=MyISAM AUTO_INCREMENT=33645 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
 
 --
 -- Table structure for table `warnings`
 --
 
+DROP TABLE IF EXISTS `warnings`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `warnings` (
-  `ID` int(255) NOT NULL,
+  `ID` int(255) NOT NULL AUTO_INCREMENT,
   `license` varchar(1024) NOT NULL,
   `reason` varchar(1024) NOT NULL,
   `staff_name` varchar(255) NOT NULL,
   `staff_steamid` varchar(255) NOT NULL,
   `time` varchar(255) NOT NULL,
-  `community` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Indexes for warnings table
---
-ALTER TABLE `warnings`
-  ADD PRIMARY KEY (`ID`),
-  MODIFY `ID` int(255) NOT NULL AUTO_INCREMENT;
-  
---
--- Indexes for bans table
---
-ALTER TABLE `bans`
-  ADD PRIMARY KEY (`ID`),
-  MODIFY `ID` int(255) NOT NULL AUTO_INCREMENT;
-  
---
--- Indexes for commend table
---
-ALTER TABLE `commend`
-  ADD PRIMARY KEY (`ID`),
-  MODIFY `ID` int(255) NOT NULL AUTO_INCREMENT;
-  
---
--- Indexes for communities table
---
-ALTER TABLE `communities`
-  ADD PRIMARY KEY (`ID`),
-  MODIFY `ID` int(255) NOT NULL AUTO_INCREMENT;
-  
---
--- Indexes for config table
---
-ALTER TABLE `config`
-  ADD PRIMARY KEY (`ID`),
-  MODIFY `ID` int(255) NOT NULL AUTO_INCREMENT;
-
---
--- Indexes for kicks table
---
-ALTER TABLE `kicks`
-  ADD PRIMARY KEY (`ID`),
-  MODIFY `ID` int(255) NOT NULL AUTO_INCREMENT;
-
---
--- Indexes for notes table
---
-ALTER TABLE `notes`
-  ADD PRIMARY KEY (`ID`),
-  MODIFY `ID` int(255) NOT NULL AUTO_INCREMENT;
-
---
--- Indexes for players table
---
-ALTER TABLE `players`
-  ADD PRIMARY KEY (`ID`),  
-  ADD UNIQUE KEY `license` (`license`,`community`),
-  MODIFY `ID` int(255) NOT NULL AUTO_INCREMENT;
-
---
--- Indexes for servers table
---
-ALTER TABLE `servers`
-  ADD PRIMARY KEY (`ID`),
-  MODIFY `ID` int(255) NOT NULL AUTO_INCREMENT;
-
---
--- Indexes for support_comments table
---
-ALTER TABLE `support_comments`
-  ADD PRIMARY KEY (`ID`),
-  MODIFY `ID` int(255) NOT NULL AUTO_INCREMENT;
-
---
--- Indexes for support_tickets table
---
-ALTER TABLE `support_tickets`
-  ADD PRIMARY KEY (`ID`),
-  MODIFY `ID` int(255) NOT NULL AUTO_INCREMENT;
-
---
--- Indexes for users table
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`ID`),
-  MODIFY `ID` int(255) NOT NULL AUTO_INCREMENT,
-  ADD UNIQUE KEY `steamid` (`steamid`);
-COMMIT;
+  `community` varchar(255) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=MyISAM AUTO_INCREMENT=32444 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-04-22 18:23:04
